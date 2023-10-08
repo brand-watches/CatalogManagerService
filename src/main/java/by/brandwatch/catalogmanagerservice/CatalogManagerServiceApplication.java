e49da8c2ddffd8dfc012a9a43ecf2ebc25139961
talalaev <talalaev4@mail.ru>
2023-10-08 20:11:14 +0300

    first commit

---
+++ src/main/java/by/brandwatch/catalogmanagerservice/CatalogManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package by.brandwatch.catalogmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogManagerServiceApplication.class, args);
+	}
+
+}
